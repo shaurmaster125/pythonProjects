700a903101463d19a9fcb540e43eed514045d959
Archey <102670907+shaurmaster125@users.noreply.github.com>
2024-10-03 15:09:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4th program
+
+a = '123.456'
+print(a)
+a = float(a)
+print(a)
+a = a*10
+print(a)
